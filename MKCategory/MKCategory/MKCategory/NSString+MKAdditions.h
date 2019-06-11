fe2bda0efc6fbcf09bbd05501a02af766811a896
liumengkai <lliumengkai@163.com>
2019-06-11 23:23:59 +0800

    添加NSString 分类

---
+++ MKCategory/MKCategory/MKCategory/NSString+MKAdditions.h
@@ -0,0 +1,17 @@
+//
+//  NSString+MKAdditions.h
+//  MKCategory
+//
+//  Created by LMK on 2019/6/11.
+//  Copyright © 2019年 LMK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (MKAdditions)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MKCategory/MKCategory/MKCategory/NSString+MKAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSString+MKAdditions.m
+//  MKCategory
+//
+//  Created by LMK on 2019/6/11.
+//  Copyright © 2019年 LMK. All rights reserved.
+//
+
+#import "NSString+MKAdditions.h"
+
+@implementation NSString (MKAdditions)
+
+@end
